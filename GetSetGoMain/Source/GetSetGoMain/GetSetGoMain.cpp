6eac90c0ae76224320fbdbff0a6a87eaad0db45b
Alexander Le <iamxanofficial@gmail.com>
2019-11-01 00:21:59 -0700

    Upgrade to UE v23.1

---
+++ GetSetGoMain/Source/GetSetGoMain/GetSetGoMain.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GetSetGoMain.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GetSetGoMain, "GetSetGoMain" );
