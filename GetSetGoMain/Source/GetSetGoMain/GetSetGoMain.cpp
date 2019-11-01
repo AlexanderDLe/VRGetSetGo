d2daf871899a1874d56ad808a2b04e039b156d29
Alexander Le <iamxanofficial@gmail.com>
2019-11-01 00:16:26 -0700

    Delete everything.

--- GetSetGoMain/Source/GetSetGoMain/GetSetGoMain.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GetSetGoMain.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GetSetGoMain, "GetSetGoMain" );
